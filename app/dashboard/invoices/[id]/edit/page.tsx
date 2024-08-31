52386d6acc797f8bbbd080b33cf8d4812074ee86
dominik.oh <olion500@daum.net>
2024-09-01 01:33:01 +0900

    fix lint

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,6 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
-import { CustomerField } from "@/app/lib/definitions";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
